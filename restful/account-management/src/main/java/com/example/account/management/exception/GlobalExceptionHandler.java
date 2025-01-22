00b3714c324a564b8157bbe29b1da44a4fce17e4
mz <ming.zhao1@nio.com>
2025-01-22 16:32:23 +0800

    add illegalargumentexception handler

--- restful/account-management/src/main/java/com/example/account/management/exception/GlobalExceptionHandler.java
+++ restful/account-management/src/main/java/com/example/account/management/exception/GlobalExceptionHandler.java
@@ -8,21 +8,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ResponseBody<String>> handleResourceNotFoundException(ResourceNotFoundException ex) {
-//        Map<String, Object> body = new HashMap<>();
-//
-//        body.put("timestamp", LocalDateTime.now());
-//        body.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
-//        body.put("message", ex.getMessage());
         ResponseBody<String> body = new ResponseBody<>(LocalDateTime.now(), ex.getMessage());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
@@ -30,12 +22,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<ResponseBody<String>> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
-//        Map<String, Object> body = new HashMap<>();
-//
-//        body.put("timestamp", LocalDateTime.now());
-//        body.put("error", HttpStatus.CONFLICT.getReasonPhrase());
-//        body.put("message", ex.getMessage());
-
         ResponseBody<String> body = new ResponseBody<>(LocalDateTime.now(), ex.getMessage());
 
         return new ResponseEntity<>(body, HttpStatus.CONFLICT);
@@ -43,20 +29,20 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseBody<String>> handleValidationException(MethodArgumentNotValidException ex) {
-//        Map<String, String> body = new HashMap<>();
-//
         String message = ex.getBindingResult().getFieldErrors().stream().map(error -> String.format("'%s': %s", error.getField(), error.getDefaultMessage())).collect(Collectors.joining("; "));
         ResponseBody<String> body = new ResponseBody<>(LocalDateTime.now(), message);
 
         return ResponseEntity.badRequest().body(body);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ResponseBody<String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ResponseBody<String> body = new ResponseBody<>(LocalDateTime.now(), ex.getMessage());
+        return ResponseEntity.badRequest().body(body);
+    }
+
     @ExceptionHandler({RuntimeException.class})
     public ResponseEntity<ResponseBody<String>> handleGenericException(RuntimeException ex) {
-//        Map<String, Object> body = new HashMap<>();
-//        body.put("timestamp", LocalDateTime.now());
-//        body.put("error", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-//        body.put("message", ex.getMessage());
         ResponseBody<String> body = new ResponseBody<>(LocalDateTime.now(), ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
